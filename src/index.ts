9035decd6cbe7524089d2d465eca44f977f2594a
Lechros <0onionion0@gmail.com>
2023-11-13 01:08:33 +0900

    fix: CustomOverlay 내보내기

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,16 @@ import MapTypeControl from './components/MapTypeControl.vue'
 import ZoomControl from './components/ZoomControl.vue'
 import Marker from './components/Marker.vue'
 import InfoWindow from './components/InfoWindow.vue'
+import CustomOverlay from './components/CustomOverlay.vue'
 import { useKakaoLoader } from './hooks/useKakaoLoader'
 import { KakaoLoader } from './utils/KakaoLoader'
-export { KakaoMap, MapTypeControl, ZoomControl, Marker, InfoWindow, useKakaoLoader, KakaoLoader }
+export {
+  KakaoMap,
+  MapTypeControl,
+  ZoomControl,
+  Marker,
+  InfoWindow,
+  CustomOverlay,
+  useKakaoLoader,
+  KakaoLoader
+}
