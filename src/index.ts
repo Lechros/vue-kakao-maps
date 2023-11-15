20f54af4b30e8f3e701394101f1d48f12e0cdd50
Lechros <0onionion0@gmail.com>
2023-11-15 13:35:26 +0900

    fix: MarkerCluster 내보내기

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
+import CustomOverlay from './components/CustomOverlay.vue'
+import InfoWindow from './components/InfoWindow.vue'
 import KakaoMap from './components/KakaoMap.vue'
 import MapTypeControl from './components/MapTypeControl.vue'
-import ZoomControl from './components/ZoomControl.vue'
 import Marker from './components/Marker.vue'
-import InfoWindow from './components/InfoWindow.vue'
-import CustomOverlay from './components/CustomOverlay.vue'
+import MarkerClusterer from './components/MarkerClusterer.vue'
 import Tileset from './components/Tileset.vue'
+import ZoomControl from './components/ZoomControl.vue'
 import { useKakaoLoader } from './hooks/useKakaoLoader'
 import { KakaoLoader } from './utils/KakaoLoader'
 export {
+  CustomOverlay,
+  InfoWindow,
+  KakaoLoader,
   KakaoMap,
   MapTypeControl,
-  ZoomControl,
   Marker,
-  InfoWindow,
-  CustomOverlay,
+  MarkerClusterer,
   Tileset,
-  useKakaoLoader,
-  KakaoLoader
+  ZoomControl,
+  useKakaoLoader
 }
