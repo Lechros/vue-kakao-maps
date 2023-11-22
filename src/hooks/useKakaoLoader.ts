ada0dbed7ca3e384594dc81672777ee267a6a328
Lechros <0onionion0@gmail.com>
2023-11-22 15:22:53 +0900

    fix: useKakaoLoader 에러 캐치

--- src/hooks/useKakaoLoader.ts
+++ src/hooks/useKakaoLoader.ts
@@ -1,7 +1,7 @@
 import { KakaoLoader, type KakaoLoaderOptions } from '@/utils/KakaoLoader'
 
 export function useKakaoLoader(options: KakaoLoaderOptions) {
-  new KakaoLoader(options).load()
+  new KakaoLoader(options).load().catch(console.error)
 
   return { loaded: KakaoLoader.loaded, error: KakaoLoader.error }
 }
